04ef3dde07870ca2b7761d74248e6955c1116b3e
zachglassman <zach.glassman@gmail.com>
2018-04-03 14:40:43 -0700

    fix typo

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ PROPHET_URL = os.environ['PROPHET_URL']
 SECRET_KEY = os.environ['SECRET_KEY']
 BOKEH_VERSION = os.environ.get('BOKEH_VERSION_NUMBER', '7')
 
-app = create_app(PROPHET_URL, SECRET_KEY, BOKEH_VERSION_NUMBER)
+app = create_app(PROPHET_URL, SECRET_KEY, BOKEH_VERSION)
 
 if __name__ == '__main__':
   app.run(port=33507)
