948f676cfa4809dd79e3a807eda1b2dcc2e3ec06
zachglassman <zach.glassman@gmail.com>
2018-04-03 14:34:02 -0700

    different env variable

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ from stockticker import create_app
 
 PROPHET_URL = os.environ['PROPHET_URL']
 SECRET_KEY = os.environ['SECRET_KEY']
+BOKEH_VERSION = os.environ.get('BOKEH_VERSION_NUMBER', '7')
 
-app = create_app(PROPHET_URL, SECRET_KEY)
+app = create_app(PROPHET_URL, SECRET_KEY, BOKEH_VERSION_NUMBER)
 
 if __name__ == '__main__':
   app.run(port=33507)
