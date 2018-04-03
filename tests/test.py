90fa60d1d884cbde2f3a574235545e5bfa905f3d
zachglassman <zach.glassman@gmail.com>
2018-04-03 13:45:57 -0700

    test success

--- tests/test.py
+++ tests/test.py
@@ -15,6 +15,16 @@ class TestApp(unittest.TestCase):
     @responses.activate
     def test_post_error(self):
         responses.add(responses.POST, self.url,
-                      json={'error': 'not found'}, status=404)
+                      json={'error': 'not found'}, 
+                      status=404)
         res = self.app.get('/')
         self.assertEqual(res.status_code, 404)
+
+    @responses.activate
+    def test_post_success(self):
+        responses.add(responses.POST, self.url,
+                      json={'ds': [1,2,3,4],
+                            'yhat': [2,3,4,5]},
+                      status=200)
+        res = self.app.get('/')
+        self.assertEqual(res.status_code, 200)
