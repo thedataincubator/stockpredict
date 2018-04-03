69f61f84d6602aa845714ecf33989a9c05a624d9
zachglassman <zach.glassman@gmail.com>
2018-04-03 13:30:18 -0700

    add a test
    update requirements
    
    removed some files
    
    clean unwanted files

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,20 @@
 import unittest
+import requests
+import responses
+from stockticker import create_app
 
-class Test(unittest.TestCase):
-    def test_one(self):
-        self.assertTrue(True)
+class TestApp(unittest.TestCase):
+
+    def setUp(self):
+        self.url = 'http://a_url'
+        self.secret_key = 'a'
+        app = create_app(self.url, self.secret_key)
+        app.testing = True
+        self.app = app.test_client()
+
+    @responses.activate
+    def test_post_error(self):
+        responses.add(responses.POST, self.url,
+                      json={'error': 'not found'}, status=404)
+        res = self.app.get('/')
+        self.assertEqual(res.status_code, 404)
