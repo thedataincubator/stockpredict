2f52876576e27673e8b04ef55d7917bd9d54ab8b
Adrian Yi <adrian.yi@mg.thedataincubator.com>
2018-04-06 18:26:04 +0000

    Added Quandl API

--- stockticker/stockticker.py
+++ stockticker/stockticker.py
@@ -1,5 +1,7 @@
 """stockticker app"""
+from datetime import datetime, timedelta
 import requests
+from requests import Timeout
 import simplejson as json
 from flask import Flask, render_template
 from flask import  request # pylint: disable=W0611
@@ -7,15 +9,39 @@ import pandas as pd
 from bokeh.plotting import figure
 from bokeh.embed import components
 
-def create_app(prophet_url, secret_key, bokeh_version):
+class QuandlException(Exception):
+    pass
+
+def query_quandl(ticker, quandl_key, value='open', days=100):
+    days = min(days, 800) # getting much larger than this causes too long URI
+
+    # prepare date string for the Quandl API
+    today = datetime.today()
+    dates = ','.join((today-timedelta(days=i)).strftime('%Y-%m-%d') for i in range(1,days+1))
+    params = {'ticker': ticker, 'date': dates, 'api_key': quandl_key}
+    try:
+        r = requests.get('https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json',
+                         params=params, timeout=10)
+    except Timeout:
+        raise QuandlException('Request timed out')
+    if r.status_code != 200:
+        raise QuandlException('Request failed with status code {}'.format(r.status_code))
+    # we can catch more exceptions below, like JSONDecodeError, KeyError, etc.
+    raw = json.loads(r.text)
+    # raw['datatable']['data'] = [['GOOGL', '2018-03-22', 1080.01, 1083.92, ...], ...]
+    df = pd.DataFrame(raw['datatable']['data'],
+                        columns=[col['name'] for col in raw['datatable']['columns']])
+    return df[['date', value]].rename({'date': 'ds', value: 'y'}, axis=1)
+
+def create_app(prophet_url, secret_key, quandl_key, bokeh_version):
     """create a flask app"""
     app = Flask(__name__)
 
     @app.route('/')
     def index(): # pylint: disable=W0612
         """main route"""
-        # Replace with API call to quandle
-        df = pd.read_csv('static/GOOGL_data.txt') # pylint: disable=C0103
+        # Add user input?
+        df = query_quandl('GOOGL', quandl_key, 'open', 100) # pylint: disable=C0103
 
         # clean up reading into parameters
         params = dict(ds=[str(i) for i in df['ds'].values],
