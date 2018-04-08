7b4f73cc0a0ae8f46ce7427aec5b33c5182a5c7c
Bo <libo9562@gmail.com>
2018-04-08 11:43:57 -0400

    Comment the checker waiting for the ticker input

--- stockticker/stockticker.py
+++ stockticker/stockticker.py
@@ -14,23 +14,28 @@ from bokeh.embed import components
 class QuandlException(Exception):
     """Exception for Quandl API"""
     pass
+def load_valid_ticker():
+    '''Load the stickers from static'''
+    with open("static/valid_tickers.txt", "r") as tickers:
+        valid_tickers = tickers.read().strip().split('\n')
+    return valid_tickers
 
 def update_valid_ticker(quandl_key):
     '''Uptate the valid ticker'''
     params = {'api_key': quandl_key}
     try:
-        r = requests.get('https://www.quandl.com/api/v3/databases/WIKI/codes',
-                         params=params, timeout=10)
+        r_z = requests.get('https://www.quandl.com/api/v3/databases/WIKI/codes',
+                           params=params, timeout=10)
     except Timeout:
         raise QuandlException('Request timed out')
-    if not r.ok:
-        raise QuandlException('Request failed with status code {}'.format(r.status_code))
-    csv_f = zipfile.ZipFile(BytesIO(r.content)).read('WIKI-datasets-codes.csv')
+    if not r_z.ok:
+        raise QuandlException('Request failed with status code {}'.format(r_z.status_code))
+    csv_f = zipfile.ZipFile(BytesIO(r_z.content)).read('WIKI-datasets-codes.csv')
     tickers = pd.read_csv(BytesIO(csv_f), names=['name', 'e'], usecols=[0],
-    squeeze=True).str.slice(start=5)
+                          squeeze=True).str.slice(start=5)
     return list(tickers.values)
 
-def check_valid_ticker(ticker,valid_tickers):
+def check_valid_ticker(ticker, valid_tickers):
     '''Validate the ticker'''
     if ticker not in valid_tickers:
         raise QuandlException('The inputed ticker is not valid')
@@ -56,13 +61,15 @@ def query_quandl(ticker, quandl_key, value='open', days=500):
 
 def create_app(prophet_url, secret_key, quandl_key, bokeh_version): # pylint: disable=W0613
     """create a flask app"""
-    valid_tickers = update_valid_ticker(quandl_key)
+    #valid_tickers = load_valid_ticker()
+    #valid_tickers = update_valid_ticker(quandl_key)
     app = Flask(__name__)
 
     @app.route('/')
     def index(): # pylint: disable=W0612
         """main route"""
         # Replace with Quandl API call on user input - may need to edit test
+        # Apply check_valid_ticker before Quandl API call
         df = pd.read_csv('static/GOOGL_data.txt') # pylint: disable=C0103
 
         # clean up reading into parameters
