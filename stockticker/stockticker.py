de4502f6fe20cca1c71882b03bac993519fbba73
Rosa Rodriguez Mota <rosarodriguezmota@Rosas-MacBook-Pro.local>
2018-04-12 16:38:00 -0400

    Fixed whitespace and imported module from absolute path

--- stockticker/stockticker.py
+++ stockticker/stockticker.py
@@ -6,7 +6,7 @@ import simplejson as json
 from flask import Flask, render_template
 from flask import  request # pylint: disable=W0611
 import pandas as pd
-from stockplot import plotting
+from .stockplot import plotting
 
 class QuandlException(Exception):
     """Exception for Quandl API"""
@@ -57,7 +57,7 @@ def create_app(prophet_url, secret_key, quandl_key, bokeh_version): # pylint: di
         prediction['ds'] = pd.to_datetime(prediction['ds'])
         df['ds'] = pd.to_datetime(df['ds'])
         # also show errors
-        script, div = plotting(prediction,df)
+        script, div = plotting(prediction, df)
         return render_template('index.html', script=script, div=div, bokeh=str(bokeh_version))
 
     return app
