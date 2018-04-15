d696eb00ab48883739f2c77d399262a7f3cdd1c0
Bo <libo9562@gmail.com>
2018-04-14 22:57:28 -0400

    use frozenset fot ticker valid

--- stockticker/stockticker.py
+++ stockticker/stockticker.py
@@ -1,6 +1,4 @@
 """stockticker app"""
-import zipfile
-from io import BytesIO
 from datetime import datetime, timedelta
 import requests
 from requests import Timeout
@@ -9,25 +7,11 @@ from flask import Flask, render_template
 from flask import  request # pylint: disable=W0611
 import pandas as pd
 from .stockplot import plotting
+from .static_ticker import VALIDT
 
 class QuandlException(Exception):
     """Exception for Quandl API"""
     pass
-
-def update_valid_ticker(quandl_key):
-    '''Uptate the valid ticker'''
-    params = {'api_key': quandl_key}
-    try:
-        r_z = requests.get('https://www.quandl.com/api/v3/databases/WIKI/codes',
-                           params=params, timeout=10)
-    except Timeout:
-        raise QuandlException('Request timed out')
-    if not r_z.ok:
-        raise QuandlException('Request failed with status code {}'.format(r_z.status_code))
-    csv_f = zipfile.ZipFile(BytesIO(r_z.content)).read('WIKI-datasets-codes.csv')
-    tickers = pd.read_csv(BytesIO(csv_f), names=['name', 'e'], usecols=[0],
-                          squeeze=True).str.slice(start=5)
-    return set(tickers.values)
 
 def query_quandl(ticker, quandl_key, value='open', days=500):
     """get stock value from quandl"""
@@ -52,13 +36,13 @@ def query_quandl(ticker, quandl_key, value='open', days=500):
 def create_app(prophet_url, secret_key, quandl_key, bokeh_version): # pylint: disable=W0613
     """create a flask app"""
     app = Flask(__name__)
-    valid_ticker = update_valid_ticker(quandl_key)
+
     @app.route('/')
     def index(): # pylint: disable=W0612
         """main route"""
         # Replace with Quandl API call on user input - may need to edit test
         ticker = 'GOOGL' # place holder
-        if ticker in valid_ticker:
+        if ticker in VALIDT:
             df = query_quandl(ticker, quandl_key) # pylint: disable=C0103
         else: # place holder
             df = query_quandl('GOOGL', quandl_key) # pylint: disable=C0103
